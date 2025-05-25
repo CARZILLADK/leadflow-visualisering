6567796ee047f6a8ba8d42dfc65b1c0036f5ee4c
CARZILLADK <rasmus@carzilla.dk>
2025-05-25 22:34:26 +0200

    Upload full project

---
+++ vitejs-vite-p4jdghxm (1)/src/App.tsx
@@ -0,0 +1,11 @@
+import LeadFlowChart from "./LeadFlowChart";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black p-6">
+      <LeadFlowChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ vitejs-vite-p4jdghxm (1)/src/LeadFlowChart.tsx
@@ -0,0 +1,1 @@
+import LeadFlowChart from "./assets/LeadFlowChart";
